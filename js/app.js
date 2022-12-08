0ffb68ac73a49753b18398c40c82b643bafc68b4
lunaF19 <104158605+lunaF19@users.noreply.github.com>
2022-12-08 22:12:39 +0000

    Deploying to gh-pages from @ lunaF19/portafolioWeb@a70de88b8986e5358a03489f9867f4acd91b0341 🚀

--- js/app.js
+++ js/app.js
@@ -25,6 +25,10 @@ sections.forEach(section => { observerHTML.observe(section) })
 // 
 const socialNets = $("#social-nets")
 
+
+const languages = ["BackEnd", "Express JS", "Oracle Pl/SQL", "FrontEnd", "React JS",]
+//const languages = ["Language 1", "Language 2", "Language 3", "Language 4"]
+const languagesIntervalSeconds = 3000
 // footer
 const footerVitaeDowload = $("#footer-vitae-dowload")
 const dowloadPdfLink = $("#dowload-pdf-link")
@@ -41,22 +45,26 @@ carouselNext()
 btnNext.addEventListener("click", carouselNext)
 btnPrev.addEventListener("click", carouselPrev)
 
+// Contact form
+const contactForm = $("#contact-form")
 
-for ( let card of cardCarouselExperience) {
+for (const card of cardCarouselExperience) {
     card.addEventListener('click', resetTimerCarousel)
-    for (let childCard of card.children) {
+    for (const childCard of card.children) {
         childCard.addEventListener('click', resetTimerCarousel)
         childCard.addEventListener('scroll', resetTimerCarousel)
     }
 }
 
+contactForm.addEventListener('submit', onSubmit)
 function resetTimerCarousel() {
     clearTimeout(timerCarousel)
-    timerCarousel = setTimeout(()=>carouselNext(), 10000 )
+    timerCarousel = setTimeout(() => carouselNext(), 10000)
 }
 
 document.addEventListener('DOMContentLoaded', function (e) {
-    intervalLanguaes(1)
+    // intervalLanguaes(1)
+    createElementsLanguages()
 
     const disableShowNavbar = linkElement => {
         linkElement.addEventListener('click', function (e) {
@@ -95,7 +103,7 @@ function carouselNext() {
     const firstCard = $(".carousel-experience-card", true)[0]
     carouselExperience.insertAdjacentElement('beforeend', firstCard)
 
-    timerCarousel = setTimeout(()=>carouselNext(), secondsTimerCarousel )
+    timerCarousel = setTimeout(() => carouselNext(), secondsTimerCarousel)
 }
 
 function carouselPrev() {
@@ -103,14 +111,80 @@ function carouselPrev() {
     const card = $(".carousel-experience-card", true)
     const lastCard = card[card.length - 1]
     carouselExperience.insertAdjacentElement('afterbegin', lastCard)
-    timerCarousel = setTimeout(()=>carouselNext(), secondsTimerCarousel )
+    timerCarousel = setTimeout(() => carouselNext(), secondsTimerCarousel)
 }
 
-// setInterval(() => carouselNext(), 5000)
+function createElementsLanguages() {
+    const laguageElement = $("#languages")
+    for (const language of languages) {
+        const spanLanguage = document.createElement('span')
+        spanLanguage.classList.add("language-item")
+        spanLanguage.innerHTML = `${language}`
+        laguageElement.appendChild(spanLanguage)
+    }
+
+    const languagesElements = $(".language-item", true)
+    languagesElements[0].classList.add("show")
+    languagesElements[1].classList.add("top")
+    languagesElements[languagesElements.length-1].classList.add("bottom")
+    setTimeout(() => intervalLanguaes2(0), languagesIntervalSeconds)
+}
+
+function intervalLanguaes2(num) {
+    // console.log("intervalLanguaes2", num)
+
+    const languagesElements = $(".language-item", true)
+    const totalLanguagesElements = languagesElements.length
+
+    const clearItem = elementHTML => {
+        elementHTML.classList.remove("show")
+        elementHTML.classList.remove("top")
+        elementHTML.classList.remove("bottom")
+    }
+    
+    let num2 = null 
+    if (num >= totalLanguagesElements-1) {
+        clearItem(languagesElements[totalLanguagesElements-1])
+        languagesElements[totalLanguagesElements-1].classList.add("bottom")
+        
+        clearItem(languagesElements[0])
+        languagesElements[0].classList.add("show")
+        
+        clearItem(languagesElements[1])
+        languagesElements[1].classList.add("top")
+        num2 = 0
+        
+    } else if (num === 0) {
+        clearItem(languagesElements[0])
+        languagesElements[0].classList.add("bottom")
+        
+        clearItem(languagesElements[1])
+        languagesElements[1].classList.add("show")
+        
+        clearItem(languagesElements[totalLanguagesElements-1])
+        languagesElements[totalLanguagesElements-1].classList.add("top")
+        num2 = 1
+        
+    } else {
+        // El elemento mostrado pasa a estar abajo
+        clearItem(languagesElements[num])
+        languagesElements[num].classList.add("bottom")
+        
+        // Elemento de arriba debe pasar a ser mostrado
+        clearItem(languagesElements[num+1])
+        languagesElements[num+1].classList.add("show")
+        
+        // Elemento de abajo pasa arriba
+        clearItem(languagesElements[num-1])
+        languagesElements[num-1].classList.add("top")
+        num2 = num + 1
+    }
+
+    setTimeout(() => intervalLanguaes2(num2), languagesIntervalSeconds)
+}
 
 function intervalLanguaes(num) {
-    const laguageElement = $("#language")
-    const languages = ["BackEnd", "Express JS", "Oracle Pl/SQL", "FrontEnd", "React JS",]
+    const laguageElement = $("#languages")
     if (num > languages.length) num = 1
     if (num < 1) num = 1
     const spanLanguage = document.createElement('span')
@@ -126,6 +200,12 @@ function intervalLanguaes(num) {
     setTimeout(() => {
         intervalLanguaes(num + 1)
     }, 3000)
+
+}
+
+function onSubmit(e) {
+    e.preventDefault()
+    alert("Falta manejar")
 
 }
 
