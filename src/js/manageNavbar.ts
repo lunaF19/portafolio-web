9bdb81305d2ad0a61b17f413a35db5b1ce68c0b4
Francisco Luna M <franj.lunam@gmail.com>
2024-01-25 15:44:21 -0600

    fix navbar, more css and less js

--- src/js/manageNavbar.ts
+++ src/js/manageNavbar.ts
@@ -1,108 +1,85 @@
-const observerHTML = new IntersectionObserver((entries) => {
-    entries.forEach(entrie => {
-        if (entrie.isIntersecting) {
-            const id = entrie.target.id
-            window.location.hash = `#${id}`;
-        }
-    })
-}, {
+let currentSection = "";
+
+const navbar = document.querySelector("#navbar");
+const socialNets = document.querySelector("#social-nets");
+const navbarBtn = document.querySelector("#navbar-btn");
+
+const observerHTML = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entrie) => {
+      if (entrie.isIntersecting) {
+        const id = entrie.target.id;
+        if (currentSection == id) return;
+        currentSection = id;
+        activeLink();
+      }
+    });
+  },
+  {
     root: document,
-    threshold: .5,
-    rootMargin: '0px 0px 20% 0px'
-})
-
-// NavBar
-
-
-// const divLinks = document.querySelector("#div-links")
-// const logoNavbar = document.querySelector("#navbar-log")
-
-
+    threshold: 0.5,
+    rootMargin: "0px 0px 20% 0px",
+  }
+);
 
 export function initManageNavBar() {
-    window.addEventListener('hashchange', function () { activeLink() });
-    const navbar = document.querySelector("#navbar")
-    const socialNets = document.querySelector("#social-nets")
+  // Se recorren las secciones para colocarlas en el observer
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    observerHTML.observe(section);
+  });
 
-    // Se recorren las secciones para colocarlas en el observer
-    const sections = document.querySelectorAll('section')
-    sections.forEach(section => { observerHTML.observe(section) })
+  navbarBtn?.addEventListener("click", function () {
+    console.log(navbar);
+    navbar?.classList.toggle("full");
+    socialNets?.classList.toggle("center");
+    socialNets?.classList.remove("full");
+  });
 
-    
-    const navbarBtn = document.querySelector("#navbar-btn")
-    navbarBtn?.addEventListener('click', function() {
-        navbar?.classList.toggle('full')
-        socialNets?.classList.toggle('center')
-        socialNets?.classList.remove('full')
-    });
-
-    (() => {
-        const disableShowNavbar = (linkElement: HTMLAnchorElement) => {
-            linkElement.addEventListener('click', function () {
-                navbar?.classList.remove('full')
-                socialNets?.classList.remove('center')
-            })
-        }
-        forEachLinks(disableShowNavbar)
-    })();
-    
-
+  (() => {
+    const disableShowNavbar = (linkElement: HTMLAnchorElement) => {
+      linkElement.addEventListener("click", function () {
+        navbar?.classList.remove("full");
+        socialNets?.classList.remove("center");
+      });
+    };
+    forEachLinks(disableShowNavbar);
+  })();
 }
 
+document.addEventListener("scroll", function () {
+  navbar?.classList.remove("full");
+  socialNets?.classList.remove("center");
 
-document.addEventListener('scroll', function () {
-    const navbar = document.querySelector("#navbar")
-    const socialNets = document.querySelector("#social-nets")
-    const navbarBtn = document.querySelector("#navbar-btn")
-    navbar?.classList.remove('full')
-    socialNets?.classList.remove('center')
-    const sectionWhois = document.querySelector("#whois")
+  const sectionWhois = document.querySelector("#whois");
 
-    if (sectionWhois) {
-        const sectionWhoisBounding: DOMRectReadOnly = sectionWhois.getBoundingClientRect()
+  if (sectionWhois) {
+    const sectionWhoisBounding: DOMRectReadOnly =
+      sectionWhois.getBoundingClientRect();
 
-        const navbarVisible = Boolean((sectionWhoisBounding.top) <= 0)
-        navbar?.classList.toggle('scroll', navbarVisible)
-        navbarBtn?.classList.toggle('active', navbarVisible)
-        socialNets?.classList.toggle('scroll', navbarVisible)
-    }
-
-
-    // manageNavBar()
-})
+    const navbarVisible = Boolean(sectionWhoisBounding.top <= 0);
+    navbar?.classList.toggle("scroll", navbarVisible);
+    navbarBtn?.classList.toggle("active", navbarVisible);
+    socialNets?.classList.toggle("scroll", navbarVisible);
+  }
+});
 
 function activeLink() {
-    //manageNavBar()
+  const toggleActiveLink = (linkElement: HTMLAnchorElement) => {
+    const hashOfLink = linkElement.href.split("#")[1];
+    const isActiveLink = Boolean(hashOfLink == currentSection);
 
-    const removeActive = (linkElement: HTMLAnchorElement) => {
-        const isActiveLink = Boolean(linkElement.href === window.location.href)
-        linkElement.classList.toggle("active", isActiveLink)
-    }
+    linkElement.classList.toggle("active", isActiveLink);
+  };
 
-    forEachLinks(removeActive)
+  forEachLinks(toggleActiveLink);
 }
 
 function forEachLinks(cb: Function) {
-    const links = document.querySelectorAll('#div-links a')
+  const links = document.querySelectorAll("#div-links a");
 
-    for (let i = 0; i < links.length; i++) {
-        const link: HTMLAnchorElement = (links[i] as HTMLAnchorElement)
-        cb(link)
-    }
+  for (let i = 0; i < links.length; i++) {
+    const link: HTMLAnchorElement = links[i] as HTMLAnchorElement;
+    cb(link);
+  }
 }
-
-
-
-/*
-function manageNavBar() {
-    const navbar = document.querySelector("#navbar")
-    const socialNets = document.querySelector("#social-nets")
-    const navbarBtn = document.querySelector("#navbar-btn")
-
-    const hasLink = window.location.hash
-    const navbarActive = ["#whois", "#experience", "#contact"].includes(hasLink)
-    navbar?.classList.toggle('scroll', navbarActive)
-    socialNets?.classList.toggle('scroll', navbarActive)
-    navbarBtn?.classList.toggle('active', navbarActive)
-}
-*/
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
-import './style/main.css'
-import { initCarousel } from './js/carouselExperience'
-import { initManageNavBar } from './js/manageNavbar'
-import { initIntervalLanguages } from './js/intervalLanguages'
+import "./style/main.css";
+import { initCarousel } from "./js/carouselExperience";
+import { initManageNavBar } from "./js/manageNavbar";
+import { initIntervalLanguages } from "./js/intervalLanguages";
 
 document.addEventListener("DOMContentLoaded", function () {
-  initCarousel()
-  initManageNavBar()
-  initIntervalLanguages()
+  initCarousel();
+  initManageNavBar();
+  initIntervalLanguages();
 
-  const footerVitaeDowload = document.querySelector("#footer-vitae-dowload")
-  const dowloadPdfLink = ( document.querySelector("#dowload-pdf-link") as HTMLElement)
+  const footerVitaeDowload = document.querySelector("#footer-vitae-dowload");
+  const dowloadPdfLink = document.querySelector(
+    "#dowload-pdf-link"
+  ) as HTMLElement;
 
-  footerVitaeDowload?.addEventListener('click', function () { dowloadPdfLink?.click() })
-})
+  footerVitaeDowload?.addEventListener("click", function () {
+    dowloadPdfLink?.click();
+  });
+});
